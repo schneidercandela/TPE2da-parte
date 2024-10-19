4d3338f4ee05af013cc3cdc845710f2b87be3303
Candela Schneider <schneider.candelagmail.com>
2024-10-19 17:37:55 -0300

    modificada base de datos

--- web2/app/model/model.db.php
+++ web2/app/model/model.db.php
@@ -17,156 +17,127 @@
             if(count($tables)==0) {
                 // Si no hay crearlas
                 $sql =<<<END
-                -- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 19-10-2024 a las 20:59:44
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `tpe`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `category`
---
-
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name_category` varchar(100) NOT NULL,
-  `season` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `category`
---
-
-INSERT INTO `category` (`id`, `name_category`, `season`) VALUES
-(1, 'Buzos', 'Invierno'),
-(3, 'Camperas', 'Otoño'),
-(5, 'Oto/Inv', 'Oto/Inv Desc');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `client`
---
-
-CREATE TABLE `client` (
-  `id` int(11) NOT NULL,
-  `username` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `rol` varchar(100) NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `client`
---
-
-INSERT INTO `client` (`id`, `username`, `password`, `rol`) VALUES
-(1, 'daniel', '$2y$10$3vD1MXuFMH70znzv0j62F./jELxiey9vevpM1R23XDG5jLbuPL.fq', 'user'),
-(3, 'webadmin', '$2y$10$5vTCBDhKh91WK.Ws3oRdLe8//FxyvvfVCUcNT7uXThEC7CF.U2UCi', 'admin'),
-(7, 'pepe', '$2y$10$2irYy8pPw7TqPrHB.haaG.KvtK1Mxwp2DZ0uyyVejh01WocxrrIna', 'user'),
-(8, 'dario', '$2y$10$3u55z21.eI1wuK/zErbyKOhw33Ytyg1oUc/Kes2B9cltNKp8t5jkO', 'user'),
-(9, 'lucas', '$2y$10$X1W.NmCNzqLLDvlB/vylA.BMietRZDC7xmv.rUT1iqnPBy6tigl4K', 'user');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `img` varchar(250) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `price` int(200) NOT NULL,
-  `fk_id_category` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `products`
---
-
-INSERT INTO `products` (`id`, `img`, `name`, `description`, `price`, `fk_id_category`) VALUES
-(1, 'https://http2.mlstatic.com/D_NQ_NP_781887-MLA69230254891_052023-O.webp', 'Buzo Adidas', 'Buzo Adidas Con Capucha y Lineas en las mangas', 17000, 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_category` (`fk_id_category`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `client`
---
-ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de la tabla `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `fk_category` FOREIGN KEY (`fk_id_category`) REFERENCES `category` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
+                --
+                -- Estructura de tabla para la tabla `category`
+                --
+                
+                CREATE TABLE `category` (
+                  `id` int(11) NOT NULL,
+                  `name_category` varchar(100) NOT NULL,
+                  `season` varchar(100) NOT NULL,
+                  `description` varchar(250) NOT NULL,
+                  `creation_date` date NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+                
+                --
+                -- Volcado de datos para la tabla `category`
+                --
+                
+                INSERT INTO `category` (`id`, `name_category`, `season`, `description`, `creation_date`) VALUES
+                (1, 'Buzos', 'Invierno', '', '0000-00-00'),
+                (2, 'Remeras', 'Verano', '', '0000-00-00'),
+                (3, 'Camperas', 'Otoño', '', '0000-00-00');
+                
+                -- --------------------------------------------------------
+                
+                --
+                -- Estructura de tabla para la tabla `client`
+                --
+                
+                CREATE TABLE `client` (
+                  `id` int(11) NOT NULL,
+                  `username` varchar(250) NOT NULL,
+                  `password` varchar(250) NOT NULL,
+                  `rol` varchar(100) NOT NULL DEFAULT 'user'
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+                
+                --
+                -- Volcado de datos para la tabla `client`
+                --
+                
+                INSERT INTO `client` (`id`, `username`, `password`, `rol`) VALUES
+                (1, 'daniel', '$2y$10$3vD1MXuFMH70znzv0j62F./jELxiey9vevpM1R23XDG5jLbuPL.fq', 'user'),
+                (3, 'webadmin', '$2y$10$5vTCBDhKh91WK.Ws3oRdLe8//FxyvvfVCUcNT7uXThEC7CF.U2UCi', 'admin'),
+                (7, 'pepe', '$2y$10$2irYy8pPw7TqPrHB.haaG.KvtK1Mxwp2DZ0uyyVejh01WocxrrIna', 'user'),
+                (8, 'dario', '$2y$10$3u55z21.eI1wuK/zErbyKOhw33Ytyg1oUc/Kes2B9cltNKp8t5jkO', 'user');
+                
+                -- --------------------------------------------------------
+                
+                --
+                -- Estructura de tabla para la tabla `products`
+                --
+                
+                CREATE TABLE `products` (
+                  `id` int(11) NOT NULL,
+                  `img` varchar(250) NOT NULL,
+                  `name` varchar(100) NOT NULL,
+                  `description` varchar(100) NOT NULL,
+                  `price` int(200) NOT NULL,
+                  `fk_id_category` int(11) NOT NULL
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+                
+                --
+                -- Volcado de datos para la tabla `products`
+                --
+                
+                INSERT INTO `products` (`id`, `img`, `name`, `description`, `price`, `fk_id_category`) VALUES
+                (1, 'https://http2.mlstatic.com/D_NQ_NP_781887-MLA69230254891_052023-O.webp', 'Buzo Adidas', 'Buzo Adidas Con Capucha y Lineas en las mangas', 17000, 1);
+                
+                --
+                -- Índices para tablas volcadas
+                --
+                
+                --
+                -- Indices de la tabla `category`
+                --
+                ALTER TABLE `category`
+                  ADD PRIMARY KEY (`id`);
+                
+                --
+                -- Indices de la tabla `client`
+                --
+                ALTER TABLE `client`
+                  ADD PRIMARY KEY (`id`);
+                
+                --
+                -- Indices de la tabla `products`
+                --
+                ALTER TABLE `products`
+                  ADD PRIMARY KEY (`id`),
+                  ADD KEY `fk_category` (`fk_id_category`);
+                
+                --
+                -- AUTO_INCREMENT de las tablas volcadas
+                --
+                
+                --
+                -- AUTO_INCREMENT de la tabla `category`
+                --
+                ALTER TABLE `category`
+                  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+                
+                --
+                -- AUTO_INCREMENT de la tabla `client`
+                --
+                ALTER TABLE `client`
+                  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+                
+                --
+                -- AUTO_INCREMENT de la tabla `products`
+                --
+                ALTER TABLE `products`
+                  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+                
+                --
+                -- Restricciones para tablas volcadas
+                --
+                
+                --
+                -- Filtros para la tabla `products`
+                --
+                ALTER TABLE `products`
+                  ADD CONSTRAINT `fk_category` FOREIGN KEY (`fk_id_category`) REFERENCES `category` (`id`);
+                COMMIT;
                 END;
                 $this->db->query($sql);
             }
